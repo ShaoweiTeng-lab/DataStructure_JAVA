015750cfaa3f31ec2646c2e7dc645071e0fbc166
JasonoTeng <sdonline13@gmail.com>
2022-12-20 08:50:15 +0800

    Finish Fivianccl

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Fibonacci {
+public class Fibonacci {
+    public void  Run(int n){
+        for(int i=0;i<n;i++)
+            System.out.println(fibonacci(i));
+    }
+    public int fibonacci(int n){
+        if(n==0)
+            return 0;
+        if (n == 1)
+            return  1;
+        return fibonacci(n-1)+fibonacci(n-2);
+
+    }
 }
